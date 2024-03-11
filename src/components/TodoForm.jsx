6827b907c06136bc16256571db9e36a30ec41f88
G-O Jacobsson <geo.jacobsson@gmail.com>
2024-03-11 20:15:07 +0100

    :lipstick: Add border style to add-button

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -20,11 +20,11 @@ const TodoForm = ({ addNewTodo }) => {
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
           placeholder="Add a new todo..."
-          className="p-2 rounded shadow-2xl border-2 border-cyan-300 mb-4 mr-4 text-black w-full"
+          className="w-full p-2 mb-4 mr-4 text-black border-2 rounded shadow-2xl border-cyan-300"
         />
         <button
           type="submit"
-          className="bg-cyan-500 text-white p-2 rounded shadow-2xl hover:bg-cyan-600 transition duration-300 ease-in-out cursor-pointer"
+          className="p-2 text-white transition duration-300 ease-in-out border-2 rounded shadow-2xl cursor-pointer bg-cyan-500 border-cyan-950 hover:bg-cyan-600"
         >
           Add Todo
         </button>
