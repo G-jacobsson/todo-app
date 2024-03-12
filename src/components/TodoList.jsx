edf3741bb7ccff4bc18a95f40e17e590c4a3ca71
G-O Jacobsson <geo.jacobsson@gmail.com>
2024-03-12 20:55:21 +0100

    :lipstick: Fix styling for filter buttons and list div

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,9 +4,9 @@ import TodoItem from './TodoItem';
 const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
   const sortedTodos = todos.sort((a, b) => a.done - b.done);
   return (
-    <div>
+    <div className="mb-20">
       <h1 className="text-3xl font-bold">Todo List</h1>
-      <ul className="mt-10 list-disc border-cyan-300 border-2 p-8 shadow-2xl rounded">
+      <ul className="p-8 mt-10 list-disc border-2 rounded shadow-2xl border-cyan-300">
         {sortedTodos.map((todo, index) => (
           <TodoItem
             todo={todo}
