dc669879d1a8e058498b9dfad6bddf64b340f3f8
G-O Jacobsson <geo.jacobsson@gmail.com>
2024-03-14 17:41:07 +0100

    :zap: Update Todo class with priority param

--- src/models/Todo.js
+++ src/models/Todo.js
@@ -1,11 +1,14 @@
+import { PRIORITY_LOW } from '../utils/constants';
 export class Todo {
   id;
   task;
   done;
+  priority;
 
-  constructor(id, task, done) {
+  constructor(id, task, done, priority = PRIORITY_LOW) {
     this.id = id;
     this.task = task;
     this.done = done;
+    this.priority = priority;
   }
 }
